d21938b684169cedfa0ea18386ee185a2998f8dc
N.Monish Kumar <137286991+monishkumarblockchain@users.noreply.github.com>
2025-04-05 11:48:38 +0530

    Add files via upload

---
+++ home.page/script.js
@@ -0,0 +1,15 @@
+function showPhotos() {
+    // Write your logic to show photos
+    alert("Photos section clicked!");
+}
+
+function showVideos() {
+    // Logic to show videos
+    alert("Videos section clicked!");
+}
+
+function open3DView() {
+    // Logic to load/open 3D viewer
+    alert("3D view clicked!");
+}
+                    
